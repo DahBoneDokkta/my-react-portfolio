8013c1a46460a4d64e778ad4a1f5f14fea0046f8
DahBoneDokkta <simon.arnar@gmail.com>
2025-03-09 01:23:40 +0100

    Finished navbar, menu button still needs to be fixed. Added some here pictures. Design for navbar and design for hero picture

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import {getImageUrl} from '../../utils';
+import { getImageUrl } from '../../utils';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
@@ -10,10 +12,15 @@ export const Navbar = () => {
       </a>
       <div className={styles.menu}>
         <img className={styles.menuBtn} 
-          src={getImageUrl("nav/menuIcon.png")} 
-          alt="menu-button/"
+          src={menuOpen 
+            ? getImageUrl("nav/closeIcon.png") 
+            : getImageUrl("nav/menuIcon.png")
+          } 
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
         />
-        <ul className={styles.menuItems}>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}>
           <li>
             <a href="#about">About</a>
           </li>
