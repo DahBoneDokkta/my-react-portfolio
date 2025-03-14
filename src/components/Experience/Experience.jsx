0aa5f7d2e2a292c7e08c897f0d0a571c20864395
DahBoneDokkta <simon.arnar@gmail.com>
2025-03-14 14:05:35 +0100

    Added Experience component. Added JSON files for data Added icon assets for skills

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import skills from '../../data/skills.json'
+import history from '../../data/history.json'
+import { getImageUrl } from '../../utils'
+
+export const Experience = () => {
+  return <section id="experience">
+    <h2>Experience</h2>
+    <div>
+        <div>{
+            skills.map((skill, id) => {
+                return (
+                    <div key={id}>
+                        <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                    </div>
+                    <p>{skill.title}</p>
+                </div>
+                );
+            })
+            }</div>
+        <ul>
+            {
+                history.map((historyItem, id) => {
+                    return <li key={id}>
+                        <img src={getImageUrl(historyItem.imageSrc)} alt="" />
+                    </li>
+                })
+            }
+        </ul>
+    </div>
+  </section>
+}
