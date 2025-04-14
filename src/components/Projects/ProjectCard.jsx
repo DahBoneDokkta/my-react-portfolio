c85ad4cb47e00c4de0ad44c3252beb344013da02
DahBoneDokkta <simon.arnar@gmail.com>
2025-04-14 19:40:26 +0200

    Finished building Project and ProjectCard Components. Fine tuning with the CSS still

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import styles from "./ProjectCard.module.css"
 import { getImageUrl } from '../../utils';
 
 export const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
     return (
-        <div>
+        <div className={styles.container}>
             <img
                 src={getImageUrl(imageSrc)}
                 alt={`Image of ${title}`}
+                className={styles.image}
             />
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <ul>{
-                skills.map((skill, id) => {
-                    return <li key={id}>{skill}</li>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.description}>{description}</p>
+            <ul className={styles.skills}>
+                {skills.map((skill, id) => {
+                    return <li key={id} className={styles.skill}>{skill}</li>
                 })}
             </ul>
-            <div>
-                <a href={demo}>Demo</a>
-                <a href={source}>Source</a>
+            <div className={styles.links}>
+                <a href={demo} className={styles.link}>Demo</a>
+                <a href={source} className={styles.link}>Source</a>
             </div>
         </div>
     );
