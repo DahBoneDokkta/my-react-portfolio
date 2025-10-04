176cf4bf5c7863f6ee5c9899ba7d07667eab1f11
DahBoneDokkta <simon.arnar@gmail.com>
2025-10-04 21:10:42 +0200

    Updated intro again, changed Contact Me button to link to LinkedIn

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,12 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Simon</h1>
-                <p className={styles.description}>I'm a soon-to-be fullstack .NET developer, graduating from Teknikhögskolan in Gävle.
+                <p className={styles.description}>I'm a fullstack .NET developer, graduated from Teknikhögskolan in Gävle May 2025.
                     Through two internships and personal projects like this portfolio, I've gained practical experience and a passion for writing clean, useful code.
                     <br />
-                    Let’s connect!
+                    <br />Let’s connect!
                 </p>
-                <a href="" className={styles.ContactBtn}>
+                <a href="https://www.linkedin.com/in/simon-arnar/" className={styles.ContactBtn}>
                     Contact Me</a>
             </div>
             <img src={getImageUrl("hero/meInLaptop.PNG")} alt="Hero image of me" className={styles.heroImg} />
